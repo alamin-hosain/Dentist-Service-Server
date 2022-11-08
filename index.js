8cf00cb95df89cd3c65e7b55cbc2ad9c0472d6a9
Hossain Alamin <hossainalamin@Hossains-MacBook-Pro.local>
2022-11-08 21:37:16 +0600

    Adding all the services

--- index.js
+++ index.js
@@ -62,6 +62,14 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { serviceId: id };
+            const cursor = AllReviewsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
     }
 
     finally {
